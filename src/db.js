c5095977e647e818484ffc4464557b436fcbe097
chandrx <chandraadam606@gmail.com>
2025-04-16 13:15:06 +0700

    hai

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "nusantara",
+  host: 'sql12.freesqldatabase.com',   // ganti dengan host kamu
+  user: 'sql1234567',                  // username dari dashboard
+  password: 'sYp4KnVdAq',            // password kamu
+  database: 'sql1234567',              // database name kamu
+  port: 3306,                           // default port
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
