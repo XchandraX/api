f4b0d31c8ad3fd9f93fdd2af680bf1de65fdf68e
chandrx <chandraadam606@gmail.com>
2025-04-16 13:34:34 +0700

    hai

--- RestApi/get_place_by_id.py
+++ RestApi/get_place_by_id.py
@@ -2,32 +2,19 @@ import pandas as pd
 import json
 import sys
 import numpy as np
-import pickle
-import os
 
-try:
-    # Mendapatkan path absolut ke direktori model
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(current_dir, '../models/recommendation_model.pkl')
-    
-    # Load model data
-    with open(model_path, 'rb') as file:
-        model_data = pickle.load(file)
-    
-    df = model_data['df']  # Ambil dataframe dari model
-    
-    place_id = int(sys.argv[1]) if len(sys.argv) > 1 else -1
-    place = df[df['place_id'] == place_id]
-    
-    if place.empty:
-        print(json.dumps(None))
-        sys.exit()
-    
-    place = place.replace({np.nan: None})
-    
-    result = place[['place_name', 'category', 'city', 'rating', 'place_description', 'place_img', 'gallery_photo_img1', 'gallery_photo_img2', 'gallery_photo_img3', 'place_map']].to_dict(orient="records")
-    
-    print(json.dumps(result[0]))
-    
-except Exception as e:
-    print(json.dumps({"error": str(e)}))
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/1vQ3QfVdNbHmUBa4kQFGa-P9kNp9-SGXJU3tBmzW0OAc/export?format=csv&id=1vQ3QfVdNbHmUBa4kQFGa-P9kNp9-SGXJU3tBmzW0OAc")
+
+place_id = int(sys.argv[1]) if len(sys.argv) > 1 else -1
+
+place = df[df['place_id'] == place_id]
+
+if place.empty:
+    print(json.dumps(None))
+    sys.exit()
+
+place = place.replace({np.nan: None})
+
+result = place[['place_name', 'category', 'city', 'rating', 'place_description', 'place_img', 'gallery_photo_img1', 'gallery_photo_img2', 'gallery_photo_img3', 'place_map']].to_dict(orient="records")
+
+print(json.dumps(result[0]))  
